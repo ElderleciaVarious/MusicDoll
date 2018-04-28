3c1722d288fe99bdbadcdd8f33ce4e84501cde67
だー <elufieramethyst@danoMacBook-puro.local>
2018-04-28 23:50:52 +0900

    add main scene and default bms loader

---
+++ Assets/Scripts/Main/MusicPlayManager.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MusicPlayManager : SingletonMonoBehaviour<MusicPlayManager> {
+
+	public GameObject noteObj, holdObj, leftObj, rightObj, lineObj, holdLineObj, frickLineObj, tapEffect, frickEffect;
+
+	public GameObject comboCounter, comboText;
+
+	public float hiSpeed = 3.0f;
+
+	private bool isPlaying;
+	private float startTime;    // 譜面開始時間
+	private bool isMusicPlaying;    // 楽曲を再生されたか
+	public float MusicOffset {  // 楽曲を再生するタイミングをずらす幅
+		set; get;
+	}
+
+	private int combo;
+
+	public MusicNotesManager notesManager;
+	public MusicTempoChange tempoManager;
+
+	private Dictionary<MusicNote, GameObject> noteObjectList;
+	private List<MusicNote> removeNoteList; // 消去するノーツをここに保持し後から消去
+	private GameObject[] holdObjects;   // ロングノーツの始点をここで記憶する
+	private GameObject[] frickObjects;   // フリックノーツの始点をここで記憶する
+
+	void Awake() {
+		
+	}
+
+	void Start () {
+		notesManager = new MusicNotesManager();
+		tempoManager = new MusicTempoChange();
+		noteObjectList = new Dictionary<MusicNote, GameObject>();
+		removeNoteList = new List<MusicNote>();
+		holdObjects = new GameObject[5];
+		frickObjects = new GameObject[2];
+
+		new TextLoader().FileLoad("re2/fumen.bms");
+		Play();
+	}
+	
+	void Update () {
+		MusicPlayUpdate();
+	}
+
+	public void Play() {
+		startTime = Time.time + 2.0f;
+		isPlaying = true;
+	}
+
+	public void Stop() {
+		isPlaying = false;
+	}
+
+	// 初期化
+	private void Init() {
+		
+	}
+
+	// 譜面を流す処理
+	private void MusicPlayUpdate() {
+
+		// 楽曲再生処理
+		if (!isMusicPlaying && Time.time - startTime > MusicOffset) {
+			GetComponent<AudioSource>().Play();
+			isMusicPlaying = true;
+		}
+
+		int nowTiming = tempoManager.GetTimingFromTime(Time.time - startTime);
+
+		// ノーツを動かす
+		float timingOffset;
+		GameObject noteObject;
+		foreach (MusicNote note in noteObjectList.Keys) {
+			noteObject = noteObjectList[note];
+
+			timingOffset = (note.TotalTiming - nowTiming) / 2000.0f * hiSpeed;
+			noteObject.SendMessage("SetPosition", timingOffset);
+
+			if (timingOffset < -0.25f) {
+				removeNoteList.Add(note);
+			}
+		}
+
+		// 過ぎ去ったノーツを消去
+		for (int i = 0; i < removeNoteList.Count; i++) {
+			if (removeNoteList[i].FrickLineGroup == -1) {	// フリックかどうか
+				Instantiate(tapEffect, noteObjectList[removeNoteList[i]].transform.position, Quaternion.identity);
+			} else {
+				Instantiate(frickEffect, noteObjectList[removeNoteList[i]].transform.position, Quaternion.identity);
+			}
+			noteObjectList[removeNoteList[i]].SendMessage("Destroy");
+			noteObjectList.Remove(removeNoteList[i]);
+			combo++;
+			comboCounter.GetComponent<Text>().text = combo.ToString();
+			comboText.GetComponent<Text>().text = "COMBO";
+		}
+		removeNoteList.Clear();
+
+		// 新しいノーツの出現を監視
+		List<MusicNote> displayNotesList = null;
+		GameObject beforeNote = null;
+		int frickLineGroup;
+		for (int i = 0; i < 5; i++) {
+			displayNotesList = notesManager.GetNoteList(i);
+
+			if (displayNotesList.Count > 0) {
+				if (displayNotesList[0].TotalTiming < nowTiming + 20000 / hiSpeed) {
+
+					GameObject newNote = null;
+					frickLineGroup = displayNotesList[0].FrickLineGroup;    // フリックかどうかを判定するために最初に算出
+
+					// ノーツを生成
+					if (holdObjects[i] == null || frickLineGroup != -1) { // 同時押し終端ではない、もしくはフリックの場合
+						newNote = Instantiate(GetNotePrefab(displayNotesList[0].Type), Vector3.up * 10f, Quaternion.identity) as GameObject;
+					} else {    // フリック以外の同時押し終端の場合はロングノーツの見た目にする
+						newNote = Instantiate(holdObj, Vector3.up * 10f, Quaternion.identity) as GameObject;
+					}
+					newNote.SendMessage("Init", i);
+
+					// 同時押し用判定
+					if (beforeNote != null) {
+						GameObject line = Instantiate(lineObj, Vector3.zero, Quaternion.identity) as GameObject;
+						line.GetComponent<TapLine>().Init(beforeNote, newNote);
+					}
+					beforeNote = newNote;
+
+					// プレハブによりノーツオブジェクトを表示
+					newNote.SendMessage("Init", i);
+					noteObjectList.Add(displayNotesList[0], newNote);
+
+					// ロングノーツ用判定
+					if (holdObjects[i] != null) {   // 終点
+						holdObjects[i].GetComponent<HoldLine>().AddEndObject(newNote);
+						holdObjects[i] = null;
+					}else if (displayNotesList[0].Type == MusicNote.NoteType.HOLD) {	 // 始点
+						GameObject line = Instantiate(holdLineObj, Vector3.zero, Quaternion.identity) as GameObject;
+						line.GetComponent<HoldLine>().Init(i, newNote);
+						holdObjects[i] = line;	// 始点を記憶
+					}
+
+					// フリック判定
+					if (displayNotesList[0].FrickLineGroup != -1) {
+						if (frickObjects[frickLineGroup] != null) {   // 終点
+							frickObjects[frickLineGroup].GetComponent<FrickLine>().AddEndObject(newNote);
+							frickObjects[frickLineGroup] = null;
+						}
+						
+						// フリック終端ではなければ補助線を出す
+						if (!displayNotesList[0].IsFrickEnd) {
+							GameObject line = Instantiate(frickLineObj, Vector3.zero, Quaternion.identity) as GameObject;
+							line.GetComponent<FrickLine>().Init(i, newNote);
+							frickObjects[frickLineGroup] = line;  // 始点を記憶
+						}
+					}
+
+					// 表示したので譜面情報リストからは消去する
+					notesManager.RemoveTopNote(i);
+				}
+			}
+		}
+	}
+
+	private GameObject GetNotePrefab(MusicNote.NoteType type) {
+		switch (type) {
+			case MusicNote.NoteType.NORMAL:
+				return noteObj;
+			case MusicNote.NoteType.HOLD:
+				return holdObj;
+			case MusicNote.NoteType.LEFTFRICK:
+				return leftObj;
+			case MusicNote.NoteType.RIGHTFRICK:
+				return rightObj;
+		}
+
+		return null;
+	}
+}
---
+++ Assets/Scripts/Main/MusicTempoChange.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MusicTempoChange{
+
+	// テンポ変化に関わる情報
+	private class TempoData {
+		private float bpm;
+		public float Bpm {
+			get { return bpm; }
+		}
+		private int timing;         // カウント
+		public int Timing {
+			get { return timing; }
+		}
+		public float TotalTime { get; set; }    // テンポが変わるまでの実時間
+
+		public TempoData(float bpm, int timing) {
+			this.bpm = bpm;
+			this.timing = timing;
+		}
+	}
+	private List<TempoData> tempoList;
+
+	public MusicTempoChange() {
+		tempoList = new List<TempoData>();
+	}
+
+	// テンポ変化を追加
+	public void Add(float bpm, int timing) {
+		tempoList.Add(new TempoData(bpm, timing));
+	}
+
+	// 指定した時間でのカウントを返す
+	public int GetTimingFromTime(float time) {
+
+		TempoData tempo = tempoList[0];
+
+		int nowTempoChangeIndex;
+		for (nowTempoChangeIndex = 1; nowTempoChangeIndex < tempoList.Count; nowTempoChangeIndex++) {
+			tempo = tempoList[nowTempoChangeIndex];
+			if (time < tempo.TotalTime) {
+				break;
+			}
+		}
+
+		tempo = tempoList[nowTempoChangeIndex - 1];
+		int timing = tempo.Timing;
+		timing += (int)((time - tempo.TotalTime) * 2400.0f * (tempo.Bpm / 60.0f));
+		return timing;
+	}
+
+	// 譜面を読み込み終えたらテンポ変化の実時間を計算する
+	public void SetTempoTotalTime() {
+
+		// 最初のBPM
+		TempoData tempo = tempoList[0];
+		float beforeBpm = tempo.Bpm;
+		tempo.TotalTime = 0.0f;
+
+		// 順にテンポ変化が起こる時間を求める
+		float totalTime = 0.0f;
+		int beforeTiming = 0;
+		for (int i = 1; i < tempoList.Count; i++) {
+			tempo = tempoList[i];
+			totalTime += (tempo.Timing - beforeTiming) / 2400.0f * (60.0f / beforeBpm);
+			tempo.TotalTime = totalTime;
+			beforeBpm = tempo.Bpm;
+			beforeTiming = tempo.Timing;
+		}
+	}
+}
